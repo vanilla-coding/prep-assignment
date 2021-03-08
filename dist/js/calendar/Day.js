2c72f71101ead7b227356cba5c4e026ca8873c3e
sounmind <sounmind777@gmail.com>
2021-03-08 09:36:30 +0900

    gitignore(dist)

--- dist/js/calendar/Day.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.DAYS = void 0;
-var DAYS = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-exports.DAYS = DAYS;
